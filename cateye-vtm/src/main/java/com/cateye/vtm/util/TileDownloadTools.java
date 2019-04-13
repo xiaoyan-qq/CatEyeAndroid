8f315822ff22a16af08db179ccb6a9123e71da53
xiaoyan-qq <xiaoyan159@qq.com>
2019-04-13 16:29:36 +0800

    增加地图下载的方法

--- cateye-vtm/src/main/java/com/cateye/vtm/util/TileDownloadTools.java
+++ cateye-vtm/src/main/java/com/cateye/vtm/util/TileDownloadTools.java
@@ -1,6 +1,11 @@
 package com.cateye.vtm.util;
 
+import android.app.Activity;
 import android.graphics.Rect;
+import android.view.View;
+
+import com.canyinghao.candialog.CanDialog;
+import com.cateye.android.vtm.R;
 
 import org.oscim.core.MercatorProjection;
 import org.oscim.core.Tile;
@@ -8,11 +13,15 @@ import org.oscim.core.Tile;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+
 public class TileDownloadTools {
     /**
      * 根据屏幕中的rect坐标计算需要下载的tile的集合
      */
-    private List<Tile> getRectLatitudeArray(Rect rect, byte currentZoomLevel, byte startZoomLevel, byte endZoomLevel) {
+    public List<Tile> getRectLatitudeArray(Rect rect, byte currentZoomLevel, byte startZoomLevel, byte endZoomLevel) {
         List<Tile> tileList = new ArrayList<>();
         long mapSize = MercatorProjection.getMapSize(currentZoomLevel);
         if (rect != null) {
@@ -43,5 +52,24 @@ public class TileDownloadTools {
             }
         }
         return tileList;
+    }
+
+
+    public void downloadTile(Tile tile){
+
+    }
+
+    /**
+     * 打开下载tile的对话框
+     * */
+    public void openDownloadTileDialog(Activity mContext, List<Tile> tileList){
+        View downloadProgressView = mContext.findViewById(R.id.tile_download_progress);
+        new CanDialog.Builder(mContext).setCancelable(false).setView(R.layout.tile_download_progress).show();
+        Observable.create(new ObservableOnSubscribe<Tile>() {
+            @Override
+            public void subscribe(ObservableEmitter<Tile> emitter) throws Exception {
+
+            }
+        })
     }
 }
